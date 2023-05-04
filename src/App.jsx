a8777cbca5a0604bdb98bfb157782f6bf2c70d8c
ardazh <127732352+ardazh@users.noreply.github.com>
2023-05-04 19:09:28 +0700

    finishing App Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react'
+
+import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [num, setNum] = React.useState(0)
+ 
   return (
     <>
-      <h1>App Counter</h1>
-      <div className="card">
-        <button className="border-2 m-1" onClick={() => setCount((count) => count + 1)}>
-          + {count}
-        </button>
-        <button className="border-2 m-1" onClick={() => setCount((count) => count - 1)}>
-          -
-        </button>
+      <div className="flex flex-col h-screen justify-center items-center gap-4" >
+      <div className="bg-orange-500 w-[100px] h-[50px] flex justify-center items-center text-white rounded-[20px]">
+      {num}
       </div>
+     <Counter onChangeNumber={setNum}/>
+      </div>
+    
     </>
   )
 }
